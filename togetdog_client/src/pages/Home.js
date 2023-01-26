9d9f5b0f2da3b22e43166090d8185a8878cc7ee7
Da Yeon Sung <sdy32@naver.com>
2023-01-26 23:53:59 +0900

    S08P12A807-166 #feat Feature:front: recoil을 활용해 상태관리로 로그인 기능 구현 추가

--- togetdog_client/src/pages/Home.js
+++ togetdog_client/src/pages/Home.js
@@ -1,10 +1,14 @@
+import { useRecoilState } from "recoil";
+import { userState } from "../recoil";
+
 const Home = () => {
-  
+  const [user, setUser] = useRecoilState(userState);
+
   return (
     <div>
-      Home
+      <div>Home {user && `안녕하세요 ${user.userName}`}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- togetdog_client/src/pages/Login.js
+++ togetdog_client/src/pages/Login.js
@@ -1,97 +1,123 @@
-import { useRef, useState } from "react"
-
-import { InputWrapper, LoginContainer, LoginWrapper, LogoWrapper, SocialLoginLogo } from "../styles/LoginEmotion"
-import { PinkBtn } from "../styles/BtnsEmotion"
-import { useRecoilState } from "recoil";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useRecoilState } from "recoil";
+
+import { BACKEND_URL } from "../config";
+
+import { InputWrapper, LoginContainer, LoginWrapper, LogoWrapper, SocialLoginLogo } from "../styles/LoginEmotion";
+import { PinkBtn } from "../styles/BtnsEmotion";
+import { userState } from "../recoil";
+
 const Login = () => {
+  const navigate = useNavigate();
 
-  // const [loginState, setLoginState] = useRecoilState(loginStatus);//리코일로 로그인 상태여부 확인
-  // const [logininfo, setLoginInfo] = useRecoilState(loginInfo);//리코일로 유저정보 관리
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
-  const navigate = useNavigate()
+  const [user, setUser] = useRecoilState(userState);
 
-  const emailRef = useRef()
-  const passwordRef = useRef()
-
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [emailError, setEmailError] = useState("")
-  const [passwordError, setPasswordError] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const onChangeEmail = (e) => {
-    setEmail(e.target.value)
-    if (emailError) setEmailError("")
-  }
+    setEmail(e.target.value);
+    if (emailError) setEmailError("");
+  };
 
   const onChangePassword = (e) => {
-    setPassword(e.target.value)
-    if (passwordError) setPasswordError("")
-  }
+    setPassword(e.target.value);
+    if (passwordError) setPasswordError("");
+  };
 
   const checkInput = () => {
-   
     if (!email) {
-      emailRef.current.focus()
-      setEmailError("이메일 입력은 필수입니다")
-      return
+      emailRef.current.focus();
+      setEmailError("이메일 입력은 필수입니다");
+      return;
     }
 
     if (!password) {
-      passwordRef.current.focus()
-      setPasswordError("비밀번호 입력은 필수입니다")
-      return
+      passwordRef.current.focus();
+      setPasswordError("비밀번호 입력은 필수입니다");
+      return;
     }
 
     if (email && password) {
-      console.log("로그인 시도 가능!")
+      console.log("로그인 시도 가능!");
+      login();
     }
+  };
 
-  }
-  
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await axios({
+        url: `${BACKEND_URL}/user/login`,
+        method: "POST",
+        data: {
+          email,
+          password,
+        },
+      });
+      console.log(data);
+
+      setEmail("");
+      setPassword("");
+      setUser(data.user);
+      console.log("로그인 성공!");
+      navigate("/");
+    } catch (e) {
+      console.log(e);
+      alert("로그인 실패");
+    }
+  };
+
   return (
     <>
-    <LoginContainer>
-      <LoginWrapper>
-        <LogoWrapper>
-          <img className="login-logo" src={process.env.PUBLIC_URL + `assets/pink_paw.png`} />
-          <div className="logo-title">ToGetDog</div>
-        </LogoWrapper>
-        <InputWrapper>
-          <div className="input-wrapper">
-            <input
-              className="email-input" 
-              ref={emailRef}
-              placeholder="이메일을 입력해주세요" 
-              onChange={onChangeEmail}
-            />
+      <LoginContainer>
+        <LoginWrapper>
+          <LogoWrapper>
+            <img className="login-logo" src={process.env.PUBLIC_URL + `assets/pink_paw.png`} />
+            <div className="logo-title">ToGetDog</div>
+          </LogoWrapper>
+          <InputWrapper>
+            <div className="input-wrapper">
+              <input
+                className="email-input"
+                ref={emailRef}
+                placeholder="이메일을 입력해주세요"
+                onChange={onChangeEmail}
+              />
+            </div>
+            <div className="error-msg">{emailError}</div>
+            <div className="input-wrapper">
+              <input
+                className="password-input"
+                type="password"
+                ref={passwordRef}
+                placeholder="비밀번호를 입력해주세요"
+                onChange={onChangePassword}
+              />
+            </div>
+            <div className="error-msg">{passwordError}</div>
+          </InputWrapper>
+          <PinkBtn onClick={checkInput}>로그인</PinkBtn>
+          <div className="social-login-wrapper">
+            <SocialLoginLogo src={process.env.PUBLIC_URL + `assets/naver.png`}></SocialLoginLogo>
+            <SocialLoginLogo src={process.env.PUBLIC_URL + `assets/kakao.png`}></SocialLoginLogo>
+            <SocialLoginLogo src={process.env.PUBLIC_URL + `assets/google.jpg`}></SocialLoginLogo>
           </div>
-          <div className="error-msg">{emailError}</div>
-          <div className="input-wrapper">
-            <input
-              className="password-input"
-              type="password" 
-              ref={passwordRef}
-              placeholder="비밀번호를 입력해주세요" 
-              onChange={onChangePassword}
-            />
+          <div className="login-bottom-wrapper">
+            <div className="login-bottom-text">회원가입</div>
+            <div className="login-bottom-text">비밀번호 찾기</div>
           </div>
-          <div className="error-msg">{passwordError}</div>
-        </InputWrapper>
-        <PinkBtn onClick={checkInput}>로그인</PinkBtn>
-        <div className="social-login-wrapper">
-          <SocialLoginLogo src={process.env.PUBLIC_URL + `assets/naver.png`}></SocialLoginLogo>
-          <SocialLoginLogo src={process.env.PUBLIC_URL + `assets/kakao.png`}></SocialLoginLogo>
-          <SocialLoginLogo src={process.env.PUBLIC_URL + `assets/google.jpg`}></SocialLoginLogo>
-        </div>
-        <div className="login-bottom-wrapper">
-          <div className="login-bottom-text">회원가입</div>
-          <div className="login-bottom-text">비밀번호 찾기</div>
-        </div>
-      </LoginWrapper>
-    </LoginContainer>
+        </LoginWrapper>
+      </LoginContainer>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
