7d252f5313b802f6a8c1ea12135069318400f547
Da Yeon Sung <sdy32@naver.com>
2023-01-27 12:35:21 +0900

    S08P12A807-166 #done Feature:front: recoil-persist 추가해서 새로고침해도 로그인 상태 유지하게 하기

--- togetdog_client/src/recoil/user.js
+++ togetdog_client/src/recoil/user.js
@@ -1,6 +1,10 @@
-import { atom } from "recoil";
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
 
 export const userState = atom({
-  key: "userState",
+  key: 'userState',
   default: null,
+  effects_UNSTABLE: [persistAtom],
 });
