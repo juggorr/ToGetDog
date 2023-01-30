4662ff54732cb80a00359ec388d3fb01167dd70f
ebing <ebing.salmon@gmail.com>
2023-01-30 12:40:11 +0900

    S08P12A807-214 #feat Feature:front: Map 페이지 Swagger와 연결

--- togetdog_client/src/pages/Map.js
+++ togetdog_client/src/pages/Map.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import {
   MapContainer,
@@ -48,9 +49,39 @@ const SinglePlace = ({ Name, Address, Type, Distance }) => {
 const Map = () => {
   // const [map, setMap] = useState(null);
   // 초기 위치 설정 (추후 사용자 위치로 설정하면 됨)
+  // const container = document.getElementById("map");
   const [curLat, setCurLat] = useState(37.501262);
   const [curLng, setCurLng] = useState(127.03962161);
+  // const [options, setOptions] = useState({
+  //   center: new kakao.maps.LatLng(curLat, curLng),
+  // });
+  // const [kakaoMap, setKakaoMap] = useState(
+  //   new kakao.maps.Map(container, options)
+  // );
+
   const [selectPlace, setSelectPlace] = useState("all");
+  const [facilities, setFacilities] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+
+  const GetFacilities = (kakaoMap) => {
+    const fetchFacilities = async () => {
+      try {
+        setFacilities(null);
+        // setError(null);
+        // setLoading(true);
+        const response = await axios.get(
+          `http://i8a807.p.ssafy.io:8081/dummy/facility?latitude=${curLat}&longitude=${curLng}`
+        );
+        setFacilities(response.data.storeList);
+      } catch (e) {
+        // setError(e);
+      }
+      // setLoading(false);
+    };
+
+    fetchFacilities();
+  };
 
   const curLocation = () => {
     if (navigator.geolocation) {
@@ -58,6 +89,7 @@ const Map = () => {
       navigator.geolocation.getCurrentPosition(function (position) {
         setCurLat(position.coords.latitude);
         setCurLng(position.coords.longitude);
+        GetFacilities();
       });
     } else {
       // HTML5의 GeoLocation을 사용할 수 없을때
@@ -70,6 +102,8 @@ const Map = () => {
     const container = document.getElementById("map");
     const options = { center: new kakao.maps.LatLng(curLat, curLng) };
     const kakaoMap = new kakao.maps.Map(container, options);
+    // setOptions({ center: new kakao.maps.LatLng(curLat, curLng) });
+    // setKakaoMap(new kakao.maps.Map(container, options));
 
     // 마커가 표시될 위치입니다
     const markerPosition = new kakao.maps.LatLng(curLat, curLng);
@@ -82,184 +116,249 @@ const Map = () => {
     // 마커가 지도 위에 표시되도록 설정합니다
     marker.setMap(kakaoMap);
 
-    // 장소 마커들 배열인데 임시로 넣어뒀고 나중에 api 받아와서 안에 넣어주면 됩니다.
-    const hospitalPositions = [
-      new kakao.maps.LatLng(37.50172241287543, 127.03890372260622),
-      new kakao.maps.LatLng(37.501852922030174, 127.03931657761372),
-      new kakao.maps.LatLng(37.498553760499505, 127.02882598822454),
-      new kakao.maps.LatLng(37.497625593121384, 127.02935713582038),
-    ];
-    const servicePositions = [
-      new kakao.maps.LatLng(37.501163694020185, 127.03919181571408),
-      new kakao.maps.LatLng(37.49671536281186, 127.03020491448352),
-      new kakao.maps.LatLng(37.496201943633714, 127.02959405469642),
-      new kakao.maps.LatLng(37.49640072567703, 127.02726459882308),
-    ];
-    const restaurantPositions = [
-      new kakao.maps.LatLng(37.501397640361894, 127.04013059928758),
-      new kakao.maps.LatLng(37.499463762912974, 127.0288828824399),
-      new kakao.maps.LatLng(37.49896834100913, 127.02833986892401),
-      new kakao.maps.LatLng(37.49893267508434, 127.02673400572665),
-    ];
-    const allPositions = [
-      new kakao.maps.LatLng(37.50114093608351, 127.03988731552987),
-    ];
+    const setMarkers = () => {
+      const hospitalPositions = [];
+      const servicePositions = [];
+      const restaurantPositions = [];
+      const allPositions = [];
+      // 테스트용 코드
+      // hospitalPositions.push(
+      //   new kakao.maps.LatLng(35.90728623800133, 127.76685537417795)
+      // );
+      // hospitalPositions.push(
+      //   new kakao.maps.LatLng(37.50186640508174, 127.03941271324857)
+      // );
+      for (let i = 0; i < facilities.length; i++) {
+        const newMarker = new kakao.maps.LatLng(
+          facilities[i].latitude,
+          facilities[i].longitude
+        );
+        if (facilities[i].type === "반려의료") {
+          hospitalPositions.push(newMarker);
+        } else if (facilities[i].type === "반려동물 서비스") {
+          servicePositions.push(newMarker);
+        } else if (facilities[i].type === "반려동물식당카페") {
+          restaurantPositions.push(newMarker);
+        } else {
+          allPositions.push(newMarker);
+        }
+      }
 
-    const hospitalMarkers = [], // 의료 마커 객체를 가지고 있을 배열입니다
-      serviceMarkers = [], // 서비스 마커 객체를 가지고 있을 배열입니다
-      restaurantMarkers = [], // 식당카페 마커 객체를 가지고 있을 배열입니다
-      allMarkers = []; // 기타 마커 객체를 가지고 있을 배열입니다
+      const hospitalMarkers = [], // 의료 마커 객체를 가지고 있을 배열입니다
+        serviceMarkers = [], // 서비스 마커 객체를 가지고 있을 배열입니다
+        restaurantMarkers = [], // 식당카페 마커 객체를 가지고 있을 배열입니다
+        allMarkers = []; // 기타 마커 객체를 가지고 있을 배열입니다
 
-    createHospitalMarkers(); // 의료 마커를 생성하고 의료 마커 배열에 추가합니다
-    createServiceMarkers(); // 반려동물 서비스 마커를 생성하고 반려동물 서비스 마커 배열에 추가합니다
-    createRestaurantMarkers(); // 식당카페 마커를 생성하고 식당카페 마커 배열에 추가합니다
-    createAllMarkers();
+      createHospitalMarkers(); // 의료 마커를 생성하고 의료 마커 배열에 추가합니다
+      createServiceMarkers(); // 반려동물 서비스 마커를 생성하고 반려동물 서비스 마커 배열에 추가합니다
+      createRestaurantMarkers(); // 식당카페 마커를 생성하고 식당카페 마커 배열에 추가합니다
+      createAllMarkers();
 
-    changeMarker("all"); // 지도에 전체 마커가 보이도록 설정합니다
+      changeMarker("all"); // 지도에 전체 마커가 보이도록 설정합니다
 
-    // 의료 마커를 생성하고 의료 마커 배열에 추가하는 함수입니다
-    function createHospitalMarkers() {
-      for (let i = 0; i < hospitalPositions.length; i++) {
-        const markerImageSrc = "../../assets/first-aid-kit.png";
-        const imageSize = new kakao.maps.Size(22, 26),
-          imageOption = { offset: new kakao.maps.Point(27, 69) };
+      // 의료 마커를 생성하고 의료 마커 배열에 추가하는 함수입니다
+      function createHospitalMarkers() {
+        for (let i = 0; i < hospitalPositions.length; i++) {
+          const markerImageSrc = "../../assets/first-aid-kit.png";
+          const imageSize = new kakao.maps.Size(22, 26),
+            imageOption = { offset: new kakao.maps.Point(27, 69) };
 
-        // 마커이미지와 마커를 생성합니다
-        const markerImage = new kakao.maps.MarkerImage(
-          markerImageSrc,
-          imageSize,
-          imageOption
+          // 마커이미지와 마커를 생성합니다
+          const markerImage = new kakao.maps.MarkerImage(
+            markerImageSrc,
+            imageSize,
+            imageOption
+          );
+
+          // 마커를 생성합니다
+          const marker = new kakao.maps.Marker({
+            position: hospitalPositions[i],
+            image: markerImage, // 마커이미지 설정
+          });
+
+          // 생성된 마커를 의료 마커 배열에 추가합니다
+          hospitalMarkers.push(marker);
+        }
+      }
+
+      // 의료 마커들의 지도 표시 여부를 설정하는 함수입니다
+      function setHospitalMarkers(kakaoMap) {
+        for (let i = 0; i < hospitalMarkers.length; i++) {
+          hospitalMarkers[i].setMap(kakaoMap);
+        }
+      }
+
+      function createServiceMarkers() {
+        for (let i = 0; i < servicePositions.length; i++) {
+          const markerImageSrc = "../../assets/scissors.png";
+          const imageSize = new kakao.maps.Size(22, 26),
+            imageOption = { offset: new kakao.maps.Point(27, 69) };
+
+          const markerImage = new kakao.maps.MarkerImage(
+            markerImageSrc,
+            imageSize,
+            imageOption
+          );
+
+          const marker = new kakao.maps.Marker({
+            position: servicePositions[i],
+            image: markerImage,
+          });
+
+          serviceMarkers.push(marker);
+        }
+      }
+
+      function setServiceMarkers(kakaoMap) {
+        for (let i = 0; i < serviceMarkers.length; i++) {
+          serviceMarkers[i].setMap(kakaoMap);
+        }
+      }
+
+      function createRestaurantMarkers() {
+        for (let i = 0; i < restaurantPositions.length; i++) {
+          const markerImageSrc = "../../assets/cutlery.png";
+          const imageSize = new kakao.maps.Size(22, 26),
+            imageOption = { offset: new kakao.maps.Point(27, 69) };
+
+          const markerImage = new kakao.maps.MarkerImage(
+            markerImageSrc,
+            imageSize,
+            imageOption
+          );
+
+          const marker = new kakao.maps.Marker({
+            position: restaurantPositions[i],
+            image: markerImage,
+          });
+
+          restaurantMarkers.push(marker);
+        }
+      }
+
+      function setRestaurantMarkers(kakaoMap) {
+        for (let i = 0; i < restaurantMarkers.length; i++) {
+          restaurantMarkers[i].setMap(kakaoMap);
+        }
+      }
+
+      function createAllMarkers() {
+        for (let i = 0; i < allPositions.length; i++) {
+          const markerImageSrc = "../../assets/cutlery.png";
+          const imageSize = new kakao.maps.Size(22, 26),
+            imageOption = { offset: new kakao.maps.Point(27, 69) };
+
+          const markerImage = new kakao.maps.MarkerImage(
+            markerImageSrc,
+            imageSize,
+            imageOption
+          );
+
+          const marker = new kakao.maps.Marker({
+            position: allPositions[i],
+            image: markerImage,
+          });
+
+          allMarkers.push(marker);
+        }
+      }
+
+      function setAllMarkers(kakaoMap) {
+        for (let i = 0; i < allMarkers.length; i++) {
+          allMarkers[i].setMap(kakaoMap);
+        }
+      }
+
+      function changeMarker() {
+        if (selectPlace === "반려의료") {
+          // 의료 마커들만 지도에 표시하도록 설정합니다
+          setHospitalMarkers(kakaoMap);
+          setServiceMarkers(null);
+          setRestaurantMarkers(null);
+          setAllMarkers(null);
+        } else if (selectPlace === "반려동물 서비스") {
+          // 반려동물 서비스 마커들만 지도에 표시하도록 설정합니다
+          setHospitalMarkers(null);
+          setServiceMarkers(kakaoMap);
+          setRestaurantMarkers(null);
+          setAllMarkers(null);
+        } else if (selectPlace === "반려동물식당카페") {
+          // 식당카페 마커들만 지도에 표시하도록 설정합니다
+          setHospitalMarkers(null);
+          setServiceMarkers(null);
+          setRestaurantMarkers(kakaoMap);
+          setAllMarkers(null);
+        } else {
+          setHospitalMarkers(kakaoMap);
+          setServiceMarkers(kakaoMap);
+          setRestaurantMarkers(kakaoMap);
+          setAllMarkers(kakaoMap);
+        }
+      }
+
+      // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+      // marker.setMap(null);
+    };
+
+    if (facilities) {
+      setMarkers();
+    }
+  }, [curLat, curLng, selectPlace, facilities]);
+
+  const GetFacilityList = () => {
+    const facilityList = [];
+
+    if (facilities) {
+      // 테스트용 코드
+      // facilityList.push(
+      //   <SinglePlace
+      //     Name={"테스트용 장소"}
+      //     Address={"전북 무주군 설천면 삼공리 942"}
+      //     Type={"반려의료"}
+      //     Distance={"2"}
+      //   />
+      // );
+      // facilityList.push(
+      //   <SinglePlace
+      //     Name={"테스트용 장소2"}
+      //     Address={"서울 강남구 역삼동 678-39"}
+      //     Type={"반려의료"}
+      //     Distance={"230"}
+      //   />
+      // );
+      for (let i = 0; i < facilities.length; i++) {
+        const curFacility = (
+          <SinglePlace
+            key={facilities[i].facilityId}
+            Name={facilities[i].facilityName}
+            Address={facilities[i].facilityAddress}
+            Type={facilities[i].type}
+            Distance={facilities[i].distance}
+          />
         );
 
-        // 마커를 생성합니다
-        const marker = new kakao.maps.Marker({
-          position: hospitalPositions[i],
-          image: markerImage, // 마커이미지 설정
-        });
-
-        // 생성된 마커를 의료 마커 배열에 추가합니다
-        hospitalMarkers.push(marker);
+        if (selectPlace === "all") {
+          facilityList.push(curFacility);
+        } else if (
+          selectPlace === "반려의료" &&
+          facilities[i].type === "반려의료"
+        ) {
+          facilityList.push(curFacility);
+        } else if (
+          selectPlace === "반려동물 서비스" &&
+          facilities[i].type === "반려동물 서비스"
+        ) {
+          facilityList.push(curFacility);
+        } else if (
+          selectPlace === "반려동물식당카페" &&
+          facilities[i].type === "반려동물식당카페"
+        ) {
+          facilityList.push(curFacility);
+        }
       }
     }
-
-    // 의료 마커들의 지도 표시 여부를 설정하는 함수입니다
-    function setHospitalMarkers(kakaoMap) {
-      for (let i = 0; i < hospitalMarkers.length; i++) {
-        hospitalMarkers[i].setMap(kakaoMap);
-      }
-    }
-
-    function createServiceMarkers() {
-      for (let i = 0; i < servicePositions.length; i++) {
-        const markerImageSrc = "../../assets/scissors.png";
-        const imageSize = new kakao.maps.Size(22, 26),
-          imageOption = { offset: new kakao.maps.Point(27, 69) };
-
-        const markerImage = new kakao.maps.MarkerImage(
-          markerImageSrc,
-          imageSize,
-          imageOption
-        );
-
-        const marker = new kakao.maps.Marker({
-          position: servicePositions[i],
-          image: markerImage,
-        });
-
-        serviceMarkers.push(marker);
-      }
-    }
-
-    function setServiceMarkers(kakaoMap) {
-      for (let i = 0; i < serviceMarkers.length; i++) {
-        serviceMarkers[i].setMap(kakaoMap);
-      }
-    }
-
-    function createRestaurantMarkers() {
-      for (let i = 0; i < restaurantPositions.length; i++) {
-        const markerImageSrc = "../../assets/cutlery.png";
-        const imageSize = new kakao.maps.Size(22, 26),
-          imageOption = { offset: new kakao.maps.Point(27, 69) };
-
-        const markerImage = new kakao.maps.MarkerImage(
-          markerImageSrc,
-          imageSize,
-          imageOption
-        );
-
-        const marker = new kakao.maps.Marker({
-          position: restaurantPositions[i],
-          image: markerImage,
-        });
-
-        restaurantMarkers.push(marker);
-      }
-    }
-
-    function setRestaurantMarkers(kakaoMap) {
-      for (let i = 0; i < restaurantMarkers.length; i++) {
-        restaurantMarkers[i].setMap(kakaoMap);
-      }
-    }
-
-    function createAllMarkers() {
-      for (let i = 0; i < allPositions.length; i++) {
-        const markerImageSrc = "../../assets/cutlery.png";
-        const imageSize = new kakao.maps.Size(22, 26),
-          imageOption = { offset: new kakao.maps.Point(27, 69) };
-
-        const markerImage = new kakao.maps.MarkerImage(
-          markerImageSrc,
-          imageSize,
-          imageOption
-        );
-
-        const marker = new kakao.maps.Marker({
-          position: allPositions[i],
-          image: markerImage,
-        });
-
-        allMarkers.push(marker);
-      }
-    }
-
-    function setAllMarkers(kakaoMap) {
-      for (let i = 0; i < allMarkers.length; i++) {
-        allMarkers[i].setMap(kakaoMap);
-      }
-    }
-
-    function changeMarker() {
-      if (selectPlace === "반려의료") {
-        // 의료 마커들만 지도에 표시하도록 설정합니다
-        setHospitalMarkers(kakaoMap);
-        setServiceMarkers(null);
-        setRestaurantMarkers(null);
-        setAllMarkers(null);
-      } else if (selectPlace === "반려동물 서비스") {
-        // 반려동물 서비스 마커들만 지도에 표시하도록 설정합니다
-        setHospitalMarkers(null);
-        setServiceMarkers(kakaoMap);
-        setRestaurantMarkers(null);
-        setAllMarkers(null);
-      } else if (selectPlace === "반려동물식당카페") {
-        // 식당카페 마커들만 지도에 표시하도록 설정합니다
-        setHospitalMarkers(null);
-        setServiceMarkers(null);
-        setRestaurantMarkers(kakaoMap);
-        setAllMarkers(null);
-      } else {
-        setHospitalMarkers(kakaoMap);
-        setServiceMarkers(kakaoMap);
-        setRestaurantMarkers(kakaoMap);
-        setAllMarkers(kakaoMap);
-      }
-    }
-
-    // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
-    // marker.setMap(null);
-  }, [curLat, curLng, selectPlace]);
+    return facilityList;
+  };
 
   return (
     <div style={{ padding: "0px 20px" }}>
@@ -307,45 +406,7 @@ const Map = () => {
           </button>
         </div>
       </PlaceButtonWrapper>
-      <div>
-        {/* 리스트 가져와서 필터링하고 띄우는 코드로 수정 필요합니다. */}
-        {selectPlace === "all" || selectPlace === "반려의료" ? (
-          // 전체 또는 의료서비스
-          <SinglePlace
-            Name="장소이름"
-            Address="서울 강남구 테헤란로 234 뉴튼프라자 101호 (삼성동)"
-            Type="반려의료"
-            Distance="343"
-          />
-        ) : null}
-        {selectPlace === "all" || selectPlace === "반려동물 서비스" ? (
-          // 전체 또는 반려동물 서비스
-          <SinglePlace
-            Name="장소이름"
-            Address="서울 강남구 테헤란로 234 뉴튼프라자 101호 (삼성동)"
-            Type="반려동물 서비스"
-            Distance="343"
-          />
-        ) : null}
-        {selectPlace === "all" || selectPlace === "반려동물식당카페" ? (
-          // 전체 또는 반려동물 식당 카페
-          <SinglePlace
-            Name="장소이름"
-            Address="서울 강남구 테헤란로 234 뉴튼프라자 101호 (삼성동)"
-            Type="반려동물식당카페"
-            Distance="343"
-          />
-        ) : null}
-        {selectPlace === "all" ? (
-          // 전체(기타 포함)
-          <SinglePlace
-            Name="장소이름"
-            Address="서울 강남구 테헤란로 234 뉴튼프라자 101호 (삼성동)"
-            Type="기타분류"
-            Distance="343"
-          />
-        ) : null}
-      </div>
+      <div>{GetFacilityList()}</div>
     </div>
   );
 };
