64e080c73fd00b9aedc4b6372ed34f9fe60083cb
ebing <ebing.salmon@gmail.com>
2023-01-30 14:46:56 +0900

    S08P12A807-154 #feat Feature:front: Walk 페이지 swagger 연결

--- togetdog_client/src/pages/Walk.js
+++ togetdog_client/src/pages/Walk.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import {
   WalkListWrapper,
   TabList,
@@ -24,9 +25,9 @@ const SingleMeeting = ({ meeting }) => {
     if (getTime === 0) {
       return "12:" + timeStr.substring(3, 5) + " AM";
     } else if (getTime < 12) {
-      return timeStr + " AM";
+      return timeStr.substring(0, 5) + " AM";
     } else if (getTime === 12) {
-      return timeStr + " PM";
+      return timeStr.substring(0, 5) + " PM";
     } else {
       return getTime - 12 + ":" + timeStr.substring(3, 5) + " PM";
     }
@@ -34,7 +35,7 @@ const SingleMeeting = ({ meeting }) => {
 
   const dayOfWeek = () => {
     const week = ["일", "월", "화", "수", "목", "금", "토"];
-    const weekStr = meeting.date.split(" ");
+    const weekStr = meeting.date.split("T");
     const dayOfTheWeek = week[new Date(weekStr[0]).getDay()];
     weekStr[0] = weekStr[0].replaceAll("-", "/");
 
@@ -96,149 +97,48 @@ const SingleMeeting = ({ meeting }) => {
   );
 };
 
-const MeetingList = ({ menu }) => {
-  // api로 받아온 약속 리스트 담기
-  const originalMeetings = [
-    {
-      partnerName: "커피중독자",
-      rating: "4.7",
-      appointmentId: "12345",
-      place: "은행나무공원",
-      date: "2023-01-19 12:00",
-      myDogs: [
-        {
-          image:
-            "https://cdn.pixabay.com/photo/2018/05/13/16/57/dog-3397110__480.jpg",
-          dogName: "뽀삐",
-        },
-        {
-          image: "choco.png",
-          dogName: "choco",
-        },
-        {
-          image: "choco.png",
-          dogName: "choco",
-        },
-      ],
-      partnerDogs: [
-        {
-          image:
-            "https://cdn.pixabay.com/photo/2018/05/13/16/57/dog-3397110__480.jpg",
-          dogName: "뽀삐",
-          dogCharacter1: "independence",
-          dogCharacter2: "active",
-          dogNeutered: "True",
-        },
-        // {
-        //   image: "choco.png",
-        //   dogName: "choco",
-        //   dogCharacter1: "independence",
-        //   dogCharacter2: "active",
-        //   dogNeutered: "True",
-        // },
-        // {
-        //   image: "choco.png",
-        //   dogName: "choco",
-        //   dogCharacter1: "independence",
-        //   dogCharacter2: "active",
-        //   dogNeutered: "True",
-        // },
-      ],
-      status: "confirmed", // confirmed, sent, received, cancelled, done
-      isRated: "False", // True, False
-    },
-    {
-      partnerName: "답이없음",
-      rating: "4.7",
-      appointmentId: "12345",
-      place: "개나리공원",
-      date: "2023-01-19 15:00",
-      myDogs: [
-        {
-          image: "choco.png",
-          dogName: "우리개",
-        },
-        {
-          image: "choco.png",
-          dogName: "우리집개",
-        },
-        {
-          image: "choco.png",
-          dogName: "내개",
-        },
-      ],
-      partnerDogs: [
-        {
-          image:
-            "https://mblogthumb-phinf.pstatic.net/20160805_32/zz0507_14703621080076bLSz_JPEG/image_2019898671470361783593.jpg?type=w800",
-          dogName: "설이",
-          dogCharacter1: "independence",
-          dogCharacter2: "active",
-          dogNeutered: "True",
-        },
-        {
-          image:
-            "https://i.pinimg.com/236x/d7/d7/5b/d7d75b636897d5d256d79b611460f640.jpg",
-          dogName: "보리",
-          dogCharacter1: "independence",
-          dogCharacter2: "active",
-          dogNeutered: "True",
-        },
-        {
-          image:
-            "https://mblogthumb-phinf.pstatic.net/MjAxNzAyMDhfMjkw/MDAxNDg2NTMxMDEzMTk2.Qzgag3njAg2rm9_uDI9l8_MCDZN_A0w9TcVdlNMQsjkg.bUqo2sYtJdCEPpzHrJUwkv_yT_xwH2fR7rXxb59--NAg.JPEG.fly5885/1_%EA%B7%80%EC%97%AC%EC%9A%B4%EA%B0%95%EC%95%84%EC%A7%80_%284%29.jpg?type=w800",
-          dogName: "해피",
-          dogCharacter1: "independence",
-          dogCharacter2: "active",
-          dogNeutered: "True",
-        },
-      ],
-      status: "confirmed", // confirmed, sent, received, cancelled, done
-      isRated: "True", // True, False
-    },
-  ];
+const MeetingListWrapper = () => {
+  const [active, setActive] = useState(1);
+  const [originalMeetings, setOriginalMeetings] = useState([]);
+
+  useEffect(() => {
+    // 임시 userId, 나중에 세션에서 받아와야 함
+    const userId = 1;
+    const url = `http://i8a807.p.ssafy.io:8081/dummy/meeting?userId=${userId}`;
+    axios
+      .get(url)
+      .then(function (response) {
+        // 데이터 들어오는 형태 확인 필요함
+        setOriginalMeetings(response.data.meetingList);
+      })
+      .catch(function (error) {});
+  }, [active]);
 
   const renderMeetings = () => {
     const meetings = [];
 
     for (let i = 0; i < originalMeetings.length; i++) {
       // 약속별로 구분하는 조건문 추가
-      if (originalMeetings[i].status === "confirmed" && menu === 1) {
-        meetings.push(
-          <SingleMeeting meeting={originalMeetings[i]}></SingleMeeting>
-        );
-      } else if (originalMeetings[i].status === "sent" && menu === 2) {
-        meetings.push(
-          <SingleMeeting meeting={originalMeetings[i]}></SingleMeeting>
-        );
-      } else if (originalMeetings[i].status === "recieved" && menu === 2) {
-        meetings.push(
-          <SingleMeeting meeting={originalMeetings[i]}></SingleMeeting>
-        );
-      } else if (originalMeetings[i].status === "cancelled" && menu === 3) {
-        meetings.push(
-          <SingleMeeting meeting={originalMeetings[i]}></SingleMeeting>
-        );
-      } else if (originalMeetings[i].status === "done" && menu === 3) {
-        meetings.push(
-          <SingleMeeting meeting={originalMeetings[i]}></SingleMeeting>
-        );
+      const singleMeet = (
+        <SingleMeeting
+          key={originalMeetings[i].appointmentId}
+          meeting={originalMeetings[i]}
+        ></SingleMeeting>
+      );
+      if (originalMeetings[i].status === "confirmed" && active === 1) {
+        meetings.push(singleMeet);
+      } else if (originalMeetings[i].status === "sent" && active === 2) {
+        meetings.push(singleMeet);
+      } else if (originalMeetings[i].status === "received" && active === 2) {
+        meetings.push(singleMeet);
+      } else if (originalMeetings[i].status === "cancelled" && active === 3) {
+        meetings.push(singleMeet);
+      } else if (originalMeetings[i].status === "done" && active === 3) {
+        meetings.push(singleMeet);
       }
     }
-
     return meetings;
   };
-
-  return (
-    <MeetingWrapper>
-      {/* meetingList 비었을 경우 처리해야함 */}
-      <div className="walkList">{renderMeetings()}</div>
-    </MeetingWrapper>
-  );
-};
-
-const MeetingListWrapper = () => {
-  const [active, setActive] = useState(1);
 
   return (
     <WalkListWrapper>
@@ -268,7 +168,9 @@ const MeetingListWrapper = () => {
           종료된 약속
         </div>
       </TabList>
-      <MeetingList menu={active}></MeetingList>
+      <MeetingWrapper>
+        <div className="walkList">{renderMeetings()}</div>
+      </MeetingWrapper>
     </WalkListWrapper>
   );
 };
