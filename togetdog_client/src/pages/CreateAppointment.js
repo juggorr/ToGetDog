9389807cc6f7ebdc28ae763d6cf9b4235517d23e
ebing <ebing.salmon@gmail.com>
2023-01-31 16:56:29 +0900

    S08P12A807-198 #feat Feature:front: 산책 요청 페이지 강아지 선택 기능 추가

--- togetdog_client/src/pages/CreateAppointment.js
+++ togetdog_client/src/pages/CreateAppointment.js
@@ -1,31 +1,13 @@
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import axios from "axios";
+import { BACKEND_URL, DUMMY_URL, LOCAL_SERVER } from "../config";
 import {
   CreateAppointmentWrapper,
   WalkRequest,
   DogImgWrapper,
 } from "../styles/CreateAppointmentEmotion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-const DogImages = (item) => {
-  const [activeDog, setActiveDog] = useState(false);
-
-  return (
-    <DogImgWrapper key={item.dogId}>
-      <div
-        className={activeDog ? "dogProfileCircle" : "dogProfileCircle disabled"}
-        onClick={() => setActiveDog(!activeDog)}
-      >
-        <img
-          className="dogProfileImg"
-          src={item.dogProfile}
-          alt="dog_profile"
-        />
-      </div>
-    </DogImgWrapper>
-  );
-};
 
 const CreateAppointment = () => {
   // 임시 아이디값, 나중에 바꿔줘야함
@@ -34,6 +16,8 @@ const CreateAppointment = () => {
 
   const [userData, setUserData] = useState({});
   const [partnerData, setPartnerData] = useState({});
+  const [myActiveDogs, setMyActiveDogs] = useState([]);
+  const [partnerActiveDogs, setPartnerActiveDogs] = useState([]);
 
   useEffect(() => {
     axios
@@ -58,6 +42,50 @@ const CreateAppointment = () => {
       });
   }, [userId, partnerId]);
 
+  const DogImages = (item, idx) => {
+    const [activeDog, setActiveDog] = useState([true, item.dog.dogId]);
+
+    useEffect(() => {
+      if (item.userKey === 1) {
+        setMyActiveDogs([...myActiveDogs, [activeDog, item.dog.dogId]]);
+      } else if (item.userKey === 2) {
+        setPartnerActiveDogs([
+          ...partnerActiveDogs,
+          [activeDog, item.dog.dogId],
+        ]);
+      }
+    }, []);
+
+    useEffect(() => {
+      if (item.userKey === 1) {
+        let tempActiveDogs = myActiveDogs;
+        tempActiveDogs[idx] = [activeDog, item.dog.dogId];
+        setMyActiveDogs(tempActiveDogs);
+      } else if (item.userKey === 2) {
+        let tempActiveDogs = partnerActiveDogs;
+        tempActiveDogs[idx] = [activeDog, item.dog.dogId];
+        setPartnerActiveDogs(tempActiveDogs);
+      }
+    }, [activeDog]);
+
+    return (
+      <DogImgWrapper key={item.dogId}>
+        <div
+          className={
+            activeDog ? "dogProfileCircle" : "dogProfileCircle disabled"
+          }
+          onClick={() => setActiveDog(!activeDog)}
+        >
+          <img
+            className="dogProfileImg"
+            src={item.dog.dogProfile}
+            alt={item.dog.dogName}
+          />
+        </div>
+      </DogImgWrapper>
+    );
+  };
+
   return (
     <CreateAppointmentWrapper>
       <div className="appointmentHeader">산책 요청하기</div>
@@ -67,16 +95,24 @@ const CreateAppointment = () => {
           {"   "}나의 강아지를 선택해주세요.
         </p>
         {userData.dog &&
-          userData.dog.map((item) => (
-            <DogImages dog={item} key={userData.dog.dogId}></DogImages>
+          userData.dog.map((item, idx) => (
+            <DogImages
+              dog={item}
+              userKey={1}
+              key={userData.dog.dogId}
+            ></DogImages>
           ))}
         <p className="queryStr">
           <FontAwesomeIcon icon="fa-user-group" />
           {"   "}상대방의 강아지를 선택해주세요.
         </p>
         {partnerData.dog &&
-          partnerData.dog.map((item) => (
-            <DogImages dog={item} key={partnerData.dog.dogId}></DogImages>
+          partnerData.dog.map((item, idx) => (
+            <DogImages
+              dog={item}
+              userKey={2}
+              key={partnerData.dog.dogId}
+            ></DogImages>
           ))}
         <p className="queryStr">
           <FontAwesomeIcon icon="fa-clock" />
