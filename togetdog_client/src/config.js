afdefbea648923c3526c6c36674892b93f2fc9df
Da Yeon Sung <sdy32@naver.com>
2023-01-31 11:16:10 +0900

    S08P12A807-165 #fix Feature:front: 서버 주소 수정

--- togetdog_client/src/config.js
+++ togetdog_client/src/config.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = '127.0.0.1';
+export const BACKEND_URL = 'https://127.0.0.1';
 export const DUMMY_URL = 'http://togetdog.site:8081/dummy';
 export const LOCAL_SERVER = 'http://70.12.247.230:8080';
