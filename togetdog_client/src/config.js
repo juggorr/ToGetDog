4e914492a760e49377a0a1035dc95dba3ed2fe1b
Da Yeon Sung <sdy32@naver.com>
2023-01-31 13:14:50 +0900

    S08P12A807-165 #fix URL 주소 수정

--- togetdog_client/src/config.js
+++ togetdog_client/src/config.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = 'http://togetdog.site:8080/api';
+export const BACKEND_URL = 'https://togetdog.site:8080/api';
 export const DUMMY_URL = 'http://togetdog.site:8081/dummy';
 export const LOCAL_SERVER = 'http://70.12.247.230:8080';
