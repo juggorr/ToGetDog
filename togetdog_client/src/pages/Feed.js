d185f573fcfc534fa385f0d66fbee91ed9cf8eaf
Da Yeon Sung <sdy32@naver.com>
2023-02-01 12:56:38 +0900

    S08P12A807-235 #style Feature:front: 레이아웃 수정 중

--- togetdog_client/src/pages/Feed.js
+++ togetdog_client/src/pages/Feed.js
@@ -1,14 +1,12 @@
 import { FeedContainer, FeedPhotoWrapper, FeedProfileWrapper } from '../styles/FeedEmotion';
-import Dog1 from '../assets/dog1.jpg';
+// import Dog1 from '../assets/dog1.jpg';
 
 const Feed = () => {
   return (
     <>
       <FeedContainer>
         <FeedProfileWrapper>프로필</FeedProfileWrapper>
-        <FeedPhotoWrapper>
-          <Feed src={Dog1} />
-        </FeedPhotoWrapper>
+        <FeedPhotoWrapper>{/* <Feed src={Dog1} /> */}</FeedPhotoWrapper>
       </FeedContainer>
     </>
   );
