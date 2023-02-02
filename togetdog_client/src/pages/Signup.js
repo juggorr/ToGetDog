0599f4cfc9b1bf68c5114110c122564d95bab154
Da Yeon Sung <sdy32@naver.com>
2023-02-02 14:54:23 +0900

    S08P12A807-244 #hotfix Feature:front: headers에 Access-Control-Allow-Origin 제거

--- togetdog_client/src/pages/Signup.js
+++ togetdog_client/src/pages/Signup.js
@@ -231,7 +231,6 @@ const Signup = () => {
         {
           headers: {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
           },
         },
       )
