463818012235f7dbb93ea70f1c0dcc08adc9bae3
kwakminju <kacamimi@naver.com>
2023-02-03 10:58:17 +0900

    Feaute-dog

--- togetdog/src/main/java/com/ssafy/togetdog/dog/model/service/DogServiceImpl.java
+++ togetdog/src/main/java/com/ssafy/togetdog/dog/model/service/DogServiceImpl.java
@@ -1,11 +1,16 @@
 package com.ssafy.togetdog.dog.model.service;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 import javax.transaction.Transactional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,6 +20,8 @@ import com.ssafy.togetdog.dog.model.entity.Dog;
 import com.ssafy.togetdog.dog.model.repository.DogRepository;
 import com.ssafy.togetdog.global.exception.InvalidInputException;
 import com.ssafy.togetdog.global.exception.UnAuthorizedException;
+import com.ssafy.togetdog.user.controller.UserRestController;
+import com.ssafy.togetdog.user.model.entity.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,8 +30,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DogServiceImpl implements DogService {
 	
-	private String filePath;
 	private final DogRepository dogRepository;
+	private final Logger logger = LoggerFactory.getLogger(DogServiceImpl.class);
+	
+	@Value("${file.path.upload-images-dogs}")
+	private String dogImageFilePath;
 	
 	/*강아지 정보 조회하기*/
 	@Override
@@ -46,18 +56,44 @@ public class DogServiceImpl implements DogService {
 
 	/*강아지 정보 등록하기*/
 	@Override
-	public void registDog(long userId, DogRegistParamDTO dogDTO, MultipartFile image) {
-		Dog dog = findDogByDogId(dogDTO.getDogId());
-		if (dog == null || image.isEmpty()) {
+	public void registDog(User user, DogRegistParamDTO dogDTO, MultipartFile image) throws IllegalStateException, IOException {
+		if (dogDTO == null || image.isEmpty()) {
 			throw new InvalidInputException();
 		}
-		if (userId != dog.getUser().getUserId()) {
+		if (user == null) {
 			throw new UnAuthorizedException();
 		}
+		String originalFileName = image.getOriginalFilename();
 		
 		String today = new SimpleDateFormat("yyMMdd").format(new Date());
-		String saveFolder = filePath + File.separator + today;
-
+		String saveFolder = dogImageFilePath + File.separator + today;
+		logger.debug("저장 폴더 : {}", saveFolder);
+		
+		File folder = new File(saveFolder);
+		if (!folder.exists()) folder.mkdirs();
+		
+		if (!originalFileName.isEmpty()) {
+				String saveFileName = UUID.randomUUID().toString() + originalFileName.substring(originalFileName.lastIndexOf('.'));
+				logger.debug("원본 파일 이름 : {}, 실제 저장 파일 이름 : {}", image.getOriginalFilename(), saveFileName);
+				image.transferTo(new File(folder, saveFileName));
+		} else {
+			throw new InvalidInputException();
+		}
+		
+		Dog dog = Dog.builder()
+				.user(user)
+				.dogName(dogDTO.getDogName())
+				.dogGender(dogDTO.getDogGender())
+				.dogType(dogDTO.getDogType())
+				.dogBirth(dogDTO.getDogBirth())
+				.dogWeight(dogDTO.getDogWeight())
+				.dogNeutered(dogDTO.isDogNeutered())
+				.dogCharacter1(dogDTO.getDogCharacter1())
+				.dogCharacter2(dogDTO.getDogCharacter2())
+				.description(dogDTO.getDescription())
+				//.dogImage(dogImage)
+				.build();
+		dogRepository.save(dog);
 				
 	}
 	
