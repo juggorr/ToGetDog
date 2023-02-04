9c083e94889ae37bb8fc8822c8763041f6c13699
isungun@naver.com <isungun@naver.com>
2023-02-05 00:16:07 +0900

    S08P12A807-220 #bugfix Feature:dev: board 불러올 때 사용되는 UserIncludesDogsDTO 수정

--- togetdog/src/main/java/com/ssafy/togetdog/user/model/dto/UserIncludesDogsDTO.java
+++ togetdog/src/main/java/com/ssafy/togetdog/user/model/dto/UserIncludesDogsDTO.java
@@ -5,12 +5,10 @@ import java.util.List;
 
 import com.ssafy.togetdog.dog.model.dto.DogInfoForUserDTO;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Builder
 @Getter
 @Setter
 @ToString
@@ -27,7 +25,6 @@ public class UserIncludesDogsDTO {
 	boolean follow;
 	int followCnt;
 	
-	@Builder
 	public UserIncludesDogsDTO(UserInfoRespDTO userInfo) {
 		LocalDate now = LocalDate.now();
 		
