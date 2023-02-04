bcbd31e31ebfad3b068527945b6ed8f5f685f00b
ebing <ebing.salmon@gmail.com>
2023-02-04 20:04:23 +0900

    S08P12A807-251 #feat Feature:front: 알림 axios로 받아오는 코드 추가

--- togetdog_client/src/pages/Notifications.js
+++ togetdog_client/src/pages/Notifications.js
@@ -1,5 +1,43 @@
+import { useEffect, useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { userState } from "../recoil";
+
+import axios from "axios";
+import { BACKEND_URL, DUMMY_URL } from "../config";
+
 const Notifications = () => {
-  return <div>Notifications</div>;
+  const navigate = useNavigate();
+  const [user, setUser] = useRecoilState(userState);
+  const [notifications, setNotifications] = useState([]);
+  const [canceled, setCanceled] = useState(false);
+  const meetingCnt = useRef(0);
+
+  useEffect(() => {
+    const getNotifications = async () => {
+      await axios
+        .get(`${DUMMY_URL}/notify`, {})
+        .then((response) => {
+          setNotifications(response.data.notifyInfo.notice);
+          setCanceled(response.data.notifyInfo.meetingCancel);
+          meetingCnt.current = response.data.notifyInfo.meetingCnt;
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+
+    getNotifications();
+  }, []);
+
+  return (
+    <div>
+      {notifications.map((item) => (
+        <p>{item.type}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Notifications;
