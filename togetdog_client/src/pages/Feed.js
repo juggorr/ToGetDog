981e0ee8ea0579c96c4e5351b21698a006899ab2
Da Yeon Sung <sdy32@naver.com>
2023-02-06 05:26:14 +0900

    S08P12A807-230 #feat Feature:front: 피드와 상세피드 navigate 연결

--- togetdog_client/src/pages/Feed.js
+++ togetdog_client/src/pages/Feed.js
@@ -279,24 +279,78 @@ const Feed = () => {
           </FeedProfileBottom>
         </FeedProfileWrapper>
         <FeedPhotoWrapper>
-          <FeedPhoto src="https://mblogthumb-phinf.pstatic.net/MjAxODA0MTVfMjY2/MDAxNTIzNzgzNTMyMTk5.XluZh00E4Hzkl1Oif19d5UPPXJqzFisXFa_3BT6sTJgg.dOueWfo5LscEpJSYAi56N7p91H_PJLM4IjOvVSexYzYg.JPEG.jjingjjing92/20180410_215717.jpg?type=w800" />
-          <FeedPhoto src="https://mypetlife.co.kr/9989/kakaotalk_20180720_165306084/" />
-          <FeedPhoto src="https://t1.daumcdn.net/cfile/tistory/9952D14D5ACEAB8831" />
-          <FeedPhoto src="https://cdn.imweb.me/thumbnail/20220105/8042079a902c2.jpg" />
-          <FeedPhoto src="https://cdn.imweb.me/thumbnail/20210512/392868a89e970.jpg" />
-          <FeedPhoto src="https://www.zooseyo.com/dog_sale/photo_free/202212/1671631945_03712600.jpg" />
-          <FeedPhoto src="https://joubebe.com/wys2/swf_upload/2017/09/27/15064845361006.png" />
-          <FeedPhoto src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTLeBn4MEtb_AqR0cj2JjQIFicoJy-tZLBdA&usqp=CAU" />
-          <FeedPhoto src="https://cdn.imweb.me/upload/S201806265b31e9f9b9a03/59074608a6b12.jpg" />
-          <FeedPhoto src="https://mblogthumb-phinf.pstatic.net/MjAxODA0MTVfMjY2/MDAxNTIzNzgzNTMyMTk5.XluZh00E4Hzkl1Oif19d5UPPXJqzFisXFa_3BT6sTJgg.dOueWfo5LscEpJSYAi56N7p91H_PJLM4IjOvVSexYzYg.JPEG.jjingjjing92/20180410_215717.jpg?type=w800" />
-          <FeedPhoto src="https://mypetlife.co.kr/9989/kakaotalk_20180720_165306084/" />
-          <FeedPhoto src="https://t1.daumcdn.net/cfile/tistory/9952D14D5ACEAB8831" />
-          <FeedPhoto src="https://cdn.imweb.me/thumbnail/20220105/8042079a902c2.jpg" />
-          <FeedPhoto src="https://cdn.imweb.me/thumbnail/20210512/392868a89e970.jpg" />
-          <FeedPhoto src="https://www.zooseyo.com/dog_sale/photo_free/202212/1671631945_03712600.jpg" />
-          <FeedPhoto src="https://joubebe.com/wys2/swf_upload/2017/09/27/15064845361006.png" />
-          <FeedPhoto src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTLeBn4MEtb_AqR0cj2JjQIFicoJy-tZLBdA&usqp=CAU" />
-          <FeedPhoto src="https://cdn.imweb.me/upload/S201806265b31e9f9b9a03/59074608a6b12.jpg" />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://mblogthumb-phinf.pstatic.net/MjAxODA0MTVfMjY2/MDAxNTIzNzgzNTMyMTk5.XluZh00E4Hzkl1Oif19d5UPPXJqzFisXFa_3BT6sTJgg.dOueWfo5LscEpJSYAi56N7p91H_PJLM4IjOvVSexYzYg.JPEG.jjingjjing92/20180410_215717.jpg?type=w800"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://mypetlife.co.kr/9989/kakaotalk_20180720_165306084/"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://t1.daumcdn.net/cfile/tistory/9952D14D5ACEAB8831"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://cdn.imweb.me/thumbnail/20220105/8042079a902c2.jpg"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://cdn.imweb.me/thumbnail/20210512/392868a89e970.jpg"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://www.zooseyo.com/dog_sale/photo_free/202212/1671631945_03712600.jpg"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://joubebe.com/wys2/swf_upload/2017/09/27/15064845361006.png"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTLeBn4MEtb_AqR0cj2JjQIFicoJy-tZLBdA&usqp=CAU"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://cdn.imweb.me/upload/S201806265b31e9f9b9a03/59074608a6b12.jpg"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://mblogthumb-phinf.pstatic.net/MjAxODA0MTVfMjY2/MDAxNTIzNzgzNTMyMTk5.XluZh00E4Hzkl1Oif19d5UPPXJqzFisXFa_3BT6sTJgg.dOueWfo5LscEpJSYAi56N7p91H_PJLM4IjOvVSexYzYg.JPEG.jjingjjing92/20180410_215717.jpg?type=w800"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://mypetlife.co.kr/9989/kakaotalk_20180720_165306084/"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://t1.daumcdn.net/cfile/tistory/9952D14D5ACEAB8831"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://cdn.imweb.me/thumbnail/20220105/8042079a902c2.jpg"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://cdn.imweb.me/thumbnail/20210512/392868a89e970.jpg"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://www.zooseyo.com/dog_sale/photo_free/202212/1671631945_03712600.jpg"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://joubebe.com/wys2/swf_upload/2017/09/27/15064845361006.png"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTLeBn4MEtb_AqR0cj2JjQIFicoJy-tZLBdA&usqp=CAU"
+          />
+          <FeedPhoto
+            onClick={() => navigate(`/board/1`)}
+            src="https://cdn.imweb.me/upload/S201806265b31e9f9b9a03/59074608a6b12.jpg"
+          />
         </FeedPhotoWrapper>
       </FeedContainer>
     </>
