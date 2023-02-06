cb4d84be7b6000d566c14fad22a7b53ec822804a
juggorr <juggorrvol.2@gmail.com>
2023-02-07 07:36:06 +0900

    #feat Feature:front: delete 요청 테스트 준비

--- togetdog_client/src/components/DeleteModal.js
+++ togetdog_client/src/components/DeleteModal.js
@@ -1,9 +1,33 @@
 import { useRef, useState } from 'react';
-import { useNavigate } from 'react-router';
+import { useNavigate, useLocation } from 'react-router';
+import axios from 'axios';
+
+
 import { MenuModalWrapper, MenuModalBody } from '../styles/ModalEmotion';
 import { SkyColorShortBtn, RedColorShortBtn } from '../styles/BtnsEmotion';
 
 function DeleteModal() {
+
+  const navigate = useNavigate();
+
+  const notDeleteDog = () => {
+    navigate('/');
+  }
+  
+  const location = useLocation();
+  const dogId = location.state.dogId;
+
+  const deleteDog = async () => {
+    
+    try {
+      await axios.delete(`http://i8a807.p.ssafy.io:8081/dummy/dog?dogId=${dogId}`)
+      console.log(`${dogId}was deleted`)
+    } catch(err) {
+      console.log(err)
+    }
+  };
+
+
 
   return (
     <MenuModalWrapper>
@@ -12,10 +36,14 @@ function DeleteModal() {
         <p>삭제한 프로필은 복구할 수 없어요.</p>
         <p>그래도 삭제하시겠어요?</p>
         <div className="two-btns-wrapper">
-          <RedColorShortBtn>
+          <RedColorShortBtn
+            onClick={notDeleteDog}
+          >
             아니오
           </RedColorShortBtn>
-          <SkyColorShortBtn>
+          <SkyColorShortBtn
+            onClick={deleteDog}
+          >
             예
           </SkyColorShortBtn>
         </div>
