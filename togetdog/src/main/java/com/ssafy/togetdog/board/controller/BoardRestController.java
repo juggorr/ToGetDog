ea37b2e708f13530d81aefcf95a8be08e80e1903
권성은 <isungun@naver.com>
2023-02-06 13:57:35 +0900

    S08P12A807-282 #hofixFeature:dev: board 페이지번호 requestparam으로 수정

--- togetdog/src/main/java/com/ssafy/togetdog/board/controller/BoardRestController.java
+++ togetdog/src/main/java/com/ssafy/togetdog/board/controller/BoardRestController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,7 +43,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.parameters.RequestParam;
 import lombok.RequiredArgsConstructor;
 
 @RestController
