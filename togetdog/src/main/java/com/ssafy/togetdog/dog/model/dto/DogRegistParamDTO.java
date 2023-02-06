ed2c692ff23047a7c6c14bf68be95ab96074c295
kwakminju <kacamimi@naver.com>
2023-02-06 09:45:05 +0900

    Feature-Back: Dog, DTO에 of method 추가로 sevice 로직을 좀 더 가시적이게 변경, file upload 테스트를 위한 임시 경로 지정

--- togetdog/src/main/java/com/ssafy/togetdog/dog/model/dto/DogRegistParamDTO.java
+++ togetdog/src/main/java/com/ssafy/togetdog/dog/model/dto/DogRegistParamDTO.java
@@ -1,11 +1,15 @@
 package com.ssafy.togetdog.dog.model.dto;
 
+import com.ssafy.togetdog.dog.model.entity.Dog;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,12 +17,32 @@ import lombok.ToString;
 @ToString
 public class DogRegistParamDTO {
 	private String dogName;
-	private String dogGender;
+	private String dogGender; 
 	private String dogType;
 	private String dogBirth;
 	private String dogWeight;
 	private boolean dogNeutered;
-	private String dogCharacter1;
-	private String dogCharacter2;
+	private String dogCharacter1; //
+	private String dogCharacter2; //
 	private String description;
+	
+	public Dog of(DogRegistParamDTO dogDTO, String image) {
+		String gender = dogDTO.getDogGender();
+		if (gender.equals("female")) gender = "f";
+		else gender = "m";
+		
+		Dog dog = Dog.builder()
+				.dogName(dogDTO.getDogName())
+				.dogGender(gender)
+				.dogType(dogDTO.getDogType())
+				.dogBirth(dogDTO.getDogBirth())
+				.dogWeight(dogDTO.getDogWeight())
+				.dogNeutered(dogDTO.isDogNeutered())
+				.dogCharacter1(dogDTO.getDogCharacter1().charAt(0) + "")
+				.dogCharacter2(dogDTO.getDogCharacter2().charAt(0) + "")
+				.description(dogDTO.getDescription())
+				.dogImage(image)
+				.build();
+		return dog;
+	}
 }
