4a0b10637c7a88001e362352103834ab34c1a14c
kwakminju <kacamimi@naver.com>
2023-02-07 11:29:47 +0900

    Feature-Back 강아지 여러마리 등록이 가능한 오류로 인해 오류 로그를 찍어보고자 exception을 자세한 cause를 찍어볼 수 있도록 변경했습니다.

--- togetdog/src/main/java/com/ssafy/togetdog/global/exception/TokenValidFailedException.java
+++ togetdog/src/main/java/com/ssafy/togetdog/global/exception/TokenValidFailedException.java
@@ -2,4 +2,8 @@ package com.ssafy.togetdog.global.exception;
 
 public class TokenValidFailedException extends RuntimeException {
 	private static final long serialVersionUID = 2238030302650813811L;
+	
+	public TokenValidFailedException(String msg) {
+		super(msg);
+	}
 }
