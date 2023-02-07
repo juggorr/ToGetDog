d19a5ec355a33cfc91d406a3c88ce3f8472abf4e
kwakminju <kacamimi@naver.com>
2023-02-07 10:22:57 +0900

    Feature-Back : dog DTO chracter 1글자로 response하던거 수정  + file load util을 file delete logic을 추가하여 file util로 변경하였습니다.

--- togetdog/src/main/java/com/ssafy/togetdog/global/util/FileUtil.java
+++ togetdog/src/main/java/com/ssafy/togetdog/global/util/FileUtil.java
@@ -18,13 +18,23 @@ import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-public class FileUploadUtil {
+public class FileUtil {
 
 	private final Logger logger = LoggerFactory.getLogger(DogServiceImpl.class);
 	
+	/***
+	 * File upload logic
+	 * @param image : 실제 업로드할 MultipartFile 객체
+	 * @param path : application.properties에 등록된 path를 @Value로 받아와서 넣어주시면 됩니다.
+	 * @return String : db에 저장될 이름을 return합니다. 받아서 db에 저장해주시면 됩니다.
+	 * @throws IllegalStateException
+	 * @throws IOException
+	 */
 	public String fileUpload(MultipartFile image, String path) throws IllegalStateException, IOException {
+		// InetAddress : 부팅시 한번만 static으로 사용하지 않으면 성능이슈가 있다고 합니다.
+		// String hostname = InetAddress.getLocalHost().getHostName();
+		
 		String originalFileName = image.getOriginalFilename();
-
 		String today = new SimpleDateFormat("yyMMdd").format(new Date());
 		String saveFolder = path + File.separator + today;
 		
@@ -41,5 +51,16 @@ public class FileUploadUtil {
 		} else {
 			throw new InvalidInputException();
 		}
+	}
+	
+	/***
+	 * File delete logic
+	 * @param dbSaveImage : DB에 저장되어있는 image 경로
+	 * @param path : application.properties에 등록된 path를 @Value로 받아와서 넣어주시면 됩니다.
+	 * @return
+	 */
+	public void fileDelete(String dbSaveImage, String path) {
+		File file = new File(path + File.separator + dbSaveImage);
+		file.delete();
 	}
 }
