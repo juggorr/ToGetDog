fe1e1649e36164da04b2c3ce44fd034c63e8c9d0
juggorr <juggorr@gmail.com>
2023-02-08 13:29:43 +0900

    #feat Feature:front: user-edit 회원정보 변경, 회원 탈퇴 구현 완료

--- togetdog_client/src/pages/UserEdit.js
+++ togetdog_client/src/pages/UserEdit.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useRecoilValue, useRecoilState, useSetRecoilState } from "recoil";
 
@@ -9,7 +9,7 @@ import SignoutConfirmModal from "../components/SignoutConfirmModal";
 import { BlackBtn, MainColorShortBtn, GreyColorShortBtn } from '../styles/BtnsEmotion';
 import OptionBtn from '../components/OptionBtn';
 import { SignupContainer, SignupWrapper, InputWrapper } from '../styles/SignupEmotion';
-import DaumKakaoAddress from '../components/DaumKakaoAddress';
+import DaumKakaoEditAddress from '../components/DaumKakaoEditAddress';
 import { authAtom, userState } from "../recoil";
 
 const genderBtnList = [
@@ -41,7 +41,6 @@ function UserEdit() {
   const [user, setUser] = useRecoilState(userState);
 
   const [userInfos, setUserInfos] =useState({});
-  const thisYear = new Date().getFullYear();
 
   useEffect(() => {
     if (!auth || !localStorage.getItem('recoil-persist')) {
@@ -50,13 +49,13 @@ function UserEdit() {
     }
     
     axios
-      .get(`${DUMMY_URL}/user/${user.userId}`, {
+      .get(`${BACKEND_URL}/user/${user.userId}`, {
         headers: {
           Authorization: auth,
         },
       })
       .then((res) => {
-        setUserInfos(res.data);
+        setUserInfos(res.data.user);
       })
       .catch((err) => {
         console.log(err)
@@ -64,37 +63,56 @@ function UserEdit() {
   }, [])
 
   // useEffect로 동기처리해버리기~
+  // placeholder값 주기
   useEffect(() => {
     setInputs({
-      ...inputs,
+      nickname: userInfos.nickName,
       gender: userInfos.userGender,
-      birth: thisYear - userInfos.userAge,
-    })
+      birth: userInfos.birth,
+      address: userInfos.address,
+      sigunguCode: userInfos.regionCode,
+    });
+    setNicknameHold(userInfos.nickName);
+    setBirthHold(userInfos.birth);
+    setAddressHold(userInfos.address);
   }, [userInfos]);
 
+  // placeholder값들 정의
+  const [nicknameHold, setNicknameHold] = useState('');
+  const [birthHold, setBirthHold] = useState('');
+  const [addressHold, setAddressHold] = useState('');
+
+  
   const [inputs, setInputs] = useState({
-    nickname: userInfos.nickName,
-    gender: userInfos.userGender, // 성별 default 값 '남자'로 설정
+    nickname: '',
+    gender: '', // 성별 default 값 '남자'로 설정
     birth: '',
-    address: userInfos.address,
-    sigunguCode: userInfos.regionCode,
+    address: '',
+    sigunguCode: '',
   });
 
   const {
-    // nickname,
+    nickname,
     gender,
     birth,
     address,
-    // sigunguCode,
+    sigunguCode,
   } = inputs;
 
-  const [nicknameError, setNicknameError] = useState(false);
+  // const [nicknameError, setNicknameError] = useState(false);
+  const nicknameError = useRef(false);
   const [nicknameErrorMsg, setNicknameErrorMsg] = useState('');
-  const [birthError, setBirthError] = useState(false);
+  // const [birthError, setBirthError] = useState(false);
+  const birthError = useRef(false);
   const [birthErrorMsg, setBirthErrorMsg] = useState('');
-  const [addressError, setAddressError] = useState(false);
+  // const [addressError, setAddressError] = useState(false);
+  const addressError = useRef(false);
   const [addressErrorMsg, setAddressErrorMsg] = useState('');
 
+  // addressError current값 바꿔주는 함수
+  const addressErrorChange = () => {
+    addressError.current = true;
+  }
 
   const onChange = (e) => {
     const { name, value } = e.target; // e.target 에서 name 과 value 를 추출
@@ -106,11 +124,11 @@ function UserEdit() {
 
   // 닉네임 핸들러 메소드
   const handleNickname = async (e) => {
+    setNicknameHold('');
     const nickname = e.target.value;
-    console.log(nickname);
     if (!nicknameKorRegexp.test(nickname) && !nicknameEngRegexp.test(nickname)) {
       console.log('닉네임 형식에 맞지 않음');
-      setNicknameError(false);
+      nicknameError.current = false;
       setNicknameErrorMsg('닉네임은 한글 1~8자 혹은 영문 2~16자');
       return;
     }
@@ -120,14 +138,14 @@ function UserEdit() {
         if (resp.status === 200) {
           console.log(resp);
           console.log('사용 가능한 닉네임');
-          setNicknameError(true);
+          nicknameError.current = true;
           setNicknameErrorMsg('사용 가능한 닉네임입니다.');
         }
       })
       .catch((err) => {
         // 409 에러일 경우로 코드 리팩토링 필요
         console.log('사용 불가능한 닉네임');
-        setNicknameError(false);
+        nicknameError.current = false;
         setNicknameErrorMsg('중복된 닉네임입니다.');
       });
 
@@ -144,12 +162,15 @@ function UserEdit() {
 
   // 출생년도 메소드
   const handleBirth = (e) => {
+    setBirthHold('');
     const birthYear = e.target.value;
     if (birthYear >= 1900 && birthYear <= new Date().getFullYear()) {
-      setBirthError(true);
+      // setBirthError(true);
+      birthError.current = true;
       setBirthErrorMsg('');
     } else {
-      setBirthError(false);
+      // setBirthError(false);
+      birthError.current = false;
       setBirthErrorMsg('출생연도를 입력해주세요.');
     }
     onChange(e);
@@ -158,6 +179,7 @@ function UserEdit() {
   const [popup, setPopup] = useState(false);
 
   const handlePopup = () => {
+    setAddressHold('');
     setPopup(!popup);
   };
 
@@ -166,24 +188,82 @@ function UserEdit() {
     navigate(`/feed/${user.userId}`)
   }
 
-  const handleEdit = () => {
+  // PUT 요청 보내기
+  const handleEdit = async () => {
     console.log('회원정보 변경!')
+
+    await axios
+      .put(
+        `${BACKEND_URL}/user`,
+        {
+          nickName: nickname,
+          gender: gender,
+          birth: birth,
+          address: address,
+          regionCode: sigunguCode,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: auth,
+          },
+        })
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+
+    navigate(`/feed/${user.userId}`);
+  };
+
+  // 값을 변경하지 않으면 placeholder를 기본값으로 설정
+  const checkHolders = () => {
+    if (nicknameHold) {
+      setInputs({
+        ...inputs,
+        nickname: nicknameHold,
+      });
+      nicknameError.current = true;
+    }
+    if (birthHold) {
+      setInputs({
+        ...inputs,
+        birth: birthHold,
+      });
+      // setBirthError(true);
+      birthError.current = true;
+    }
+    if (addressHold) {
+      setInputs({
+        ...inputs,
+        address: addressHold,
+      });
+      // setAddressError(true);
+      addressError.current = true;
+    }
+    return;
+    // console.log('바꿔')
   }
 
   const checkOthers = () => {
-    if (!birthError) {
-      setBirthError(false);
+    if (!birthError.current) {
+      // setBirthError(false);
+      birthError.current = false;
       setBirthErrorMsg('출생연도를 입력해주세요.');
     }
-    if (!addressError) {
-      setAddressError(false);
+    if (!addressError.current) {
+      // setAddressError(false);
+      addressError.current = false;
       setAddressErrorMsg('주소를 입력해주세요.');
     } else {
-      setAddressError(true);
+      // setAddressError(true);
+      addressError.current = true;
       setAddressErrorMsg('');
     }
 
-    if (nicknameError && birthError && addressError) {
+    if (nicknameError.current && birthError.current && addressError.current) {
       console.log('회원 정보 변경을 시도합니다.');
       handleEdit();
     } else {
@@ -191,6 +271,11 @@ function UserEdit() {
       return;
     }
   };
+
+  const checkEdit = () => {
+    checkHolders();
+    checkOthers();
+  }
 
   const handleSignout = () => {
     setSignoutBtnClick(true);
@@ -214,7 +299,7 @@ function UserEdit() {
             <input
               name='nickname'
               className='email-input'
-              placeholder={userInfos?.nickName}
+              placeholder={nicknameHold}
               onChange={(e) => handleNickname(e)}
             />
           </div>
@@ -250,7 +335,7 @@ function UserEdit() {
                 className='number-input' 
                 name='birth' 
                 onChange={(e) => handleBirth(e)} 
-                placeholder={userInfos? birth : thisYear} 
+                placeholder={birthHold} 
               />
             </div>
             <div className='year'>년</div>
@@ -267,22 +352,22 @@ function UserEdit() {
               <input 
                 className='email-input' 
                 value={address} name='address' 
-                placeholder={userInfos?.address} 
+                placeholder={addressHold} 
                 disabled 
               />
             </div>
             <BlackBtn onClick={handlePopup}>주소 찾기</BlackBtn>
             {popup && (
-              <DaumKakaoAddress
+              <DaumKakaoEditAddress
                 data={inputs}
                 setData={setInputs}
                 error={addressError}
-                setError={setAddressError}
+                setError={addressErrorChange}
                 errorMsg={addressErrorMsg}
                 setErrorMsg={setAddressErrorMsg}
                 popup={popup}
                 setPopup={setPopup}
-              ></DaumKakaoAddress>
+              ></DaumKakaoEditAddress>
             )}
           </div>
           <div className='error'>{addressErrorMsg}</div>
@@ -290,7 +375,7 @@ function UserEdit() {
         <div className='signup-desc'>* 표시는 필수 입력 값입니다.</div>
         <div className='two-btns-wrapper'>
             <GreyColorShortBtn onClick={handleNotEdit}>돌아가기</GreyColorShortBtn>
-            <MainColorShortBtn onClick={checkOthers}>변경하기</MainColorShortBtn>
+            <MainColorShortBtn onClick={checkEdit}>변경하기</MainColorShortBtn>
         </div>
         <div className='edit-bottom-wrapper'>
           <div 
