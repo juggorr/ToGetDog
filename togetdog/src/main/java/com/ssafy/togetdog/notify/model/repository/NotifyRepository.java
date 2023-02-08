d482e23cb53ef0d7af0453d535c082916ad63610
kwakminju <kacamimi@naver.com>
2023-02-08 09:12:26 +0900

    Feature-Back: Notify에 paging 기능을 더했습니다.

--- togetdog/src/main/java/com/ssafy/togetdog/notify/model/repository/NotifyRepository.java
+++ togetdog/src/main/java/com/ssafy/togetdog/notify/model/repository/NotifyRepository.java
@@ -3,13 +3,20 @@ package com.ssafy.togetdog.notify.model.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.togetdog.notify.model.entity.Notify;
 import com.ssafy.togetdog.user.model.entity.User;
 
 public interface NotifyRepository extends JpaRepository<Notify, Long> {
-	Optional<List<Notify>> findAllByReceiverAndNotifyTypeInOrderByNotifyDateDesc(User user, List<String> notifyTypes);
+	// 좋아요, 팔로우 알림 리스트를 반환합니다.
+	Page<Notify> findAllByReceiverAndNotifyTypeInOrderByNotifyDateDesc(User user, List<String> notifyTypes, Pageable pageable);
+	// 약속 취소 알람 리스트 반환 -> 전부 삭제처리 하기 위해서
 	Optional<List<Notify>> findAllByReceiverAndNotifyTypeAndCheck(User user, String notifyType, boolean check);
+	// 약속 취소 알람 갯수 반환
 	Long countByReceiverAndNotifyTypeAndCheck(User user, String notifyType, boolean check);
+	
+	Optional<List<Notify>> findAllByReceiver(User user);
 }
