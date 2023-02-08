2d6591370571b3f33ac61fe942faa227c01e200a
kwakminju <kacamimi@naver.com>
2023-02-08 17:38:44 +0900

    Feature-Back : 아직 수락하지 않은 받은 산책 요청의 개수를 알림 response에 추가했습니다.

--- togetdog/src/main/java/com/ssafy/togetdog/appointment/model/repository/AppointmentRepository.java
+++ togetdog/src/main/java/com/ssafy/togetdog/appointment/model/repository/AppointmentRepository.java
@@ -3,7 +3,6 @@ package com.ssafy.togetdog.appointment.model.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -18,7 +17,8 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
 
 	List<Appointment> findAllBySentUserOrReceivedUser(User userOne, User userTwo);
 	
-//	@Query(value = "delete from notify n where n.receiver_user_id=:userId and n.notify_type='c'", nativeQuery = true)
+	Long countByReceivedUserAndStatus(User user, String status);
+	
 	@Query(value = "delete from notify n where n.receiver_user_id=:userId and n.notify_type='c'", nativeQuery = true)
 	void deleteAppointmentCancelNotifyByUserId(@Param("userId") long userId);
 }
