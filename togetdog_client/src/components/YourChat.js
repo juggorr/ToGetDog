d0797d2418fb0adaec2e0a6090501b1d71c8b91a
Da Yeon Sung <sdy32@naver.com>
2023-02-09 14:41:30 +0900

    S08P12A807-324 #feat Feature:front: 채팅 API 연결 완료

--- togetdog_client/src/components/YourChat.js
+++ togetdog_client/src/components/YourChat.js
@@ -1,11 +1,36 @@
-import { YourChatBox, YourChatBoxWrapper } from "../styles/ChatEmotion";
+import { YourChatBox, YourChatBoxWrapper } from '../styles/ChatEmotion';
 
-const YourChat = ({ text }) => {
+const YourChat = ({ time, text }) => {
+  const timeForToday = (value) => {
+    let today = new Date();
+    // console.log(today);
+    const timeValue = new Date(value);
+    // console.log(timeValue);
+
+    const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
+    if (betweenTime < 1) return '방금전';
+    if (betweenTime < 60) {
+      return `${betweenTime}분전`;
+    }
+
+    const betweenTimeHour = Math.floor(betweenTime / 60);
+    if (betweenTimeHour < 24) {
+      return `${betweenTimeHour}시간전`;
+    }
+
+    const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+    if (betweenTimeDay < 365) {
+      return `${betweenTimeDay}일전`;
+    }
+
+    return `${Math.floor(betweenTimeDay / 365)}년전`;
+  };
+
   return (
     <>
       <YourChatBoxWrapper>
         <YourChatBox>{text}</YourChatBox>
-        <div className="time-txt">2일전</div>
+        <div className='time-txt'>{timeForToday(time)}</div>
       </YourChatBoxWrapper>
     </>
   );
