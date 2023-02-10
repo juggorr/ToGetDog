98a037f18cb20799040f55a442f7b67bc87c5f07
juggorr <juggorr@gmail.com>
2023-02-10 16:25:35 +0900

    #feat Feature:front: social-login 소셜닉네임 검증

--- togetdog_client/src/pages/Oauth.js
+++ togetdog_client/src/pages/Oauth.js
@@ -3,6 +3,18 @@ import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 // 이페이지에서 Params에 토큰이 들어옴
+// location params에서 토큰짤라서 들고잇기
+// useSetRecoilState 이용해서 토큰 설정해주기
+// 
+
+// 로그인에 사용되는 코드
+// 
+// setUser(resp.data.user);
+// // 로그인 토큰 설정 (추후에 주석 해제 예정)
+// const userToken = resp.data['access-token'];
+// localStorage.setItem('user', JSON.stringify(userToken));
+// setAuth(userToken);
+// navigate('/');
 
 function Oauth() {
 
