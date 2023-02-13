04cae985aa0ff05a4f3c6df150647de646125375
권성은 <isungun@naver.com>
2023-02-14 00:00:57 +0900

    git commit revert

--- togetdog/src/main/java/com/ssafy/togetdog/chat/repository/ChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.togetdog.chat.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssafy.togetdog.chat.model.entity.ChatRoom;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>{
-
-}
--- togetdog/src/main/java/com/ssafy/togetdog/global/exception/ExcessNumberOfDogsException.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.togetdog.global.exception;
-
-public class ExcessNumberOfDogsException extends RuntimeException {
-	private static final long serialVersionUID = -1242345;
-	
-	public ExcessNumberOfDogsException(String msg) {
-		super(msg);
-	}
-}
