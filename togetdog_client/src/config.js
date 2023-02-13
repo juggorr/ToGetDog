55323fbcdf4cebc708e12d22bd56f209cf648c0e
Da Yeon Sung <sdy32@naver.com>
2023-02-13 14:39:54 +0900

    #hotfix Feature:front: 강아지 없을 때 피드 안뜨는 버그 수정

--- togetdog_client/src/config.js
+++ togetdog_client/src/config.js
@@ -1,4 +1,4 @@
-// export const BACKEND_URL = 'https://togetdog.site/api';
-export const BACKEND_URL = window.location.origin + '/api';
+export const BACKEND_URL = 'https://i8a807.p.ssafy.io/api';
+// export const BACKEND_URL = window.location.origin + '/api';
 // export const DUMMY_URL = 'https://togetdog.site/dummy';
 export const DUMMY_URL = window.location.origin + '/dummy';
