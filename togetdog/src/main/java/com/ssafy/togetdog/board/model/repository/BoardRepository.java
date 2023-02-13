6b522accc55c4330552def0573a79fdb521a3436
권성은 <isungun@naver.com>
2023-02-13 17:14:42 +0900

    S08P12A807-359 #bugfix 보드 sort 방식 수정

--- togetdog/src/main/java/com/ssafy/togetdog/board/model/repository/BoardRepository.java
+++ togetdog/src/main/java/com/ssafy/togetdog/board/model/repository/BoardRepository.java
@@ -11,10 +11,10 @@ import com.ssafy.togetdog.board.model.entity.Board;
 import com.ssafy.togetdog.dog.model.entity.Dog;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-
 	Board getByBoardId(long boardId);
 	Page<Board> findAllByDog(Dog dog, Pageable pageable);
 	Page<BoardHomeDTO> findAllByDogIn(List<Dog> dogList, Pageable pageable);
 	void deleteAllByDog(Dog dog);
 	List<Board> findAllByDog(Dog dog);
+	Page<Board> findAll(Pageable pageable);
 }
