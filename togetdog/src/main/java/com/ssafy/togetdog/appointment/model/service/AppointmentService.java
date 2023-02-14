d6e78aff0c1a4839c158c925aa3a14cc57d77ebc
권성은 <isungun@naver.com>
2023-02-14 14:40:23 +0900

    S08P12A807-356 #bugfix 정적분석 null 포인트 날 수 있는 곳 수정

--- togetdog/src/main/java/com/ssafy/togetdog/appointment/model/service/AppointmentService.java
+++ togetdog/src/main/java/com/ssafy/togetdog/appointment/model/service/AppointmentService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.hibernate.query.criteria.internal.expression.ConcatExpression;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Sort;
@@ -35,7 +36,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AppointmentService {
 	private final Logger logger = LoggerFactory.getLogger(AppointmentService.class);
-	
+
 	private final AppointmentRepository appointmentRepository;
 	private final SentAppointmentRepository sentAppointmentRepository;
 	private final ReceivedAppointmentRepository receivedAppointmentRepository;
@@ -46,42 +47,46 @@ public class AppointmentService {
 	public List<AppointmentListDTO> findAllByUserId(long userId) {
 		User user = new User();
 		user.setUserId(userId);
-		
-		List<Appointment> reqlist = appointmentRepository.findAllBySentUserOrReceivedUser(user, user, Sort.by("dateTime").ascending());
-		
-		List<AppointmentListDTO> requestListTime = reqlist.stream()
-				.map(a-> AppointmentListDTO.of(a)).collect(Collectors.toList());
+
+		List<Appointment> reqlist = appointmentRepository.findAllBySentUserOrReceivedUser(user, user,
+				Sort.by("dateTime").ascending());
+
+		List<AppointmentListDTO> requestListTime = reqlist.stream().map(a -> AppointmentListDTO.of(a))
+				.collect(Collectors.toList());
 		logger.info("requestList============== : {}", requestListTime);
 		LocalDateTime now = LocalDateTime.now();
-		List<AppointmentListDTO> requestList = new ArrayList<AppointmentListDTO>(); 
+		List<AppointmentListDTO> requestList = new ArrayList<AppointmentListDTO>();
 //		logger.info("현재 시간 ============== : {}", now);
-		
+
 		for (int i = 0; i < requestListTime.size(); i++) {
 			logger.info("requestList of i============== : {}", requestListTime.get(i).getDateTime().compareTo(now));
-			if(requestListTime.get(i).getStatus().equals("wait") && requestListTime.get(i).getDateTime().compareTo(now) < 0) {
+			if (requestListTime.get(i).getStatus().equals("wait")
+					&& requestListTime.get(i).getDateTime().compareTo(now) < 0) {
 				deleteAppointment(requestListTime.get(i).getRoomId());
 			}
 			requestList.add(requestListTime.get(i));
 		}
-		
+
 		// 약속 리스트
 		for (int i = 0; i < requestList.size(); i++) {
-			List<DogInfoRespDTO> sentDogs = new ArrayList<>(); 
+			List<DogInfoRespDTO> sentDogs = new ArrayList<>();
 			List<DogInfoRespDTO> recvDogs = new ArrayList<>();
 
 			logger.info("requestList of i============== : {}", requestList.get(i));
 			List<SentAppointment> sentApps = sentAppointmentRepository.findAllByAppointment(reqlist.get(i));
 			for (SentAppointment sent : sentApps) {
-				if(sent.getDog().getDogName().equals("deleted")) continue;
+				if (sent.getDog().getDogName().equals("deleted"))
+					continue;
 				logger.info("============== : {}", DogInfoRespDTO.of(sent.getDog()));
 				sentDogs.add(DogInfoRespDTO.of(sent.getDog(), Double.parseDouble(sent.getDog().getDogWeight())));
 			}
 			logger.info("!============== : {}", sentDogs);
 			requestList.get(i).setUserOneDogs(sentDogs);
-			
+
 			List<ReceivedAppointment> recvApps = receivedAppointmentRepository.findAllByAppointment(reqlist.get(i));
 			for (ReceivedAppointment recv : recvApps) {
-				if(recv.getDog().getDogName().equals("deleted")) continue;
+				if (recv.getDog().getDogName().equals("deleted"))
+					continue;
 				logger.info("============== : {}", DogInfoRespDTO.of(recv.getDog()));
 				recvDogs.add(DogInfoRespDTO.of(recv.getDog(), Double.parseDouble(recv.getDog().getDogWeight())));
 			}
@@ -90,10 +95,9 @@ public class AppointmentService {
 		}
 		logger.info("return appointment sentList : {}", requestList.size());
 		logger.info("return appointment sentList : {}", requestList);
-		
+
 		/*
-		 * //recv, sent 분리된 리스트 
-		 * User user = new User(); user.setUserId(userId);
+		 * //recv, sent 분리된 리스트 User user = new User(); user.setUserId(userId);
 		 * List<Appointment> sentlist = appointmentRepository.findAllBySentUser(user);
 		 * List<Appointment> recvlist =
 		 * appointmentRepository.findAllByReceivedUser(user);
@@ -130,7 +134,7 @@ public class AppointmentService {
 		 * recvList.get(i).setReceivedDogs(recvDogs); }
 		 * logger.info("return appointment recvList : {}", recvList.size());
 		 * logger.info("return appointment recvList : {}", recvList);
-		 */		
+		 */
 		return requestList;
 	}
 
@@ -144,44 +148,46 @@ public class AppointmentService {
 		apInfoRegistDTO.setStatus("wait");
 		Appointment appointment = apInfoRegistDTO.toAppointment();
 		appointmentRepository.save(appointment);
-		
+
 		logger.info("return appointment roomId : {}", appointment.getRoomId());
 		for (Dog dog : myDogs) {
-			sentAppointmentRepository.save(apInfoRegistDTO
-					.toSentAppointment(appointment.getRoomId(), dog.getDogId()));
+			sentAppointmentRepository.save(apInfoRegistDTO.toSentAppointment(appointment.getRoomId(), dog.getDogId()));
 		}
-		
+
 		for (Dog dog : partnerDogs) {
-			receivedAppointmentRepository.save(apInfoRegistDTO
-					.toReceivedAppointment(appointment.getRoomId(), dog.getDogId()));
+			receivedAppointmentRepository
+					.save(apInfoRegistDTO.toReceivedAppointment(appointment.getRoomId(), dog.getDogId()));
 		}
-		
-		
+
 	}
 
 	public void updateAppointment(long roomId, String status) {
 		Appointment appointment = appointmentRepository.findById(roomId).orElse(null);
-		appointment.setStatus(status);
-		appointmentRepository.save(appointment);
-		
+		if (appointment != null) {
+			appointment.setStatus(status);
+			appointmentRepository.save(appointment);
+		}
+
 	}
 
 	public void deleteAppointment(long roomId) {
 		Optional<Appointment> appointment = appointmentRepository.findById(roomId);
-		//entity mapping이 안돼서 하위 속성 삭제 먼저함
+		// entity mapping이 안돼서 하위 속성 삭제 먼저함
 		receivedAppointmentRepository.deleteByAppointment(appointment);
 		sentAppointmentRepository.deleteByAppointment(appointment);
-		appointmentRepository.deleteById(roomId);		
+		appointmentRepository.deleteById(roomId);
 	}
 
 	public void rateAppointment(long userId, long roomId, int rating) {
 		Appointment appointment = appointmentRepository.findById(roomId).orElse(null);
+		if(appointment == null) return;
 		logger.info("rateAppointment ==== appointemnt : {}", appointment);
-		if(appointment.getSentUser().getUserId() == userId) {
-			if(appointment.isSenderRated()) return;
+		if (appointment.getSentUser().getUserId() == userId) {
+			if (appointment.isSenderRated())
+				return;
 			long bSum = appointment.getReceivedUser().getRatingSum();
 			long aSum = bSum;
-			if(rating == 0) { // 산책 약속 "noshow"했을 경우
+			if (rating == 0) { // 산책 약속 "noshow"했을 경우
 				aSum -= 5;
 			} else {
 				aSum += rating;
@@ -193,10 +199,11 @@ public class AppointmentService {
 			appointment.setSenderRated(true);
 			appointmentRepository.save(appointment);
 		} else {
-			if(appointment.isReceiverRated()) return;
+			if (appointment.isReceiverRated())
+				return;
 			long bSum = appointment.getSentUser().getRatingSum();
 			long aSum = bSum;
-			if(rating == 0) { // 산책 약속 "noshow"했을 경우
+			if (rating == 0) { // 산책 약속 "noshow"했을 경우
 				aSum -= 5;
 			} else {
 				aSum += rating;
@@ -213,36 +220,36 @@ public class AppointmentService {
 	// 동네 친구 추천
 	public List<DogRecommendListDTO> recommendFriendsForDog(long userId, long dogId) {
 		User user = userRepository.findById(userId).orElse(null);
+		if(user == null) return null;
 		String regionCode = user.getRegionCode();
-		
+
 		Dog dog = dogRepository.findByDogId(dogId);
 		DogInfoRespDTO myDog = DogInfoRespDTO.of(dog, Double.parseDouble(dog.getDogWeight()));
 		logger.info("myDog ============== : {}", myDog);
 		boolean neutured = myDog.isDogNeutered();
 		int age = myDog.getDogAge() / 12;
 		LocalDateTime now = LocalDateTime.now();
-		String tenYearBefore = String.valueOf(now.minusYears(10)).substring(0, 4) + String.valueOf(now.minusYears(10)).substring(5, 7);
-		String fiveYearBefore = String.valueOf(now.minusYears(5)).substring(0, 4) + String.valueOf(now.minusYears(5)).substring(5, 7);
-		String oneYearBefore = String.valueOf(now.minusYears(1)).substring(0, 4) + String.valueOf(now.minusYears(1)).substring(5, 7);
+		String tenYearBefore = String.valueOf(now.minusYears(10)).substring(0, 4)
+				+ String.valueOf(now.minusYears(10)).substring(5, 7);
+		String fiveYearBefore = String.valueOf(now.minusYears(5)).substring(0, 4)
+				+ String.valueOf(now.minusYears(5)).substring(5, 7);
+		String oneYearBefore = String.valueOf(now.minusYears(1)).substring(0, 4)
+				+ String.valueOf(now.minusYears(1)).substring(5, 7);
 		String thisYear = String.valueOf(now).substring(0, 4) + String.valueOf(now).substring(5, 7);
 		logger.info("age ============== : {}", age);
 		double weight = myDog.getDogWeight();
 		String startWeight = "-1";
 		String endWeight = "-1";
 		String gender = myDog.getDogGender().substring(0, 1);
-		
+
 		/*
-		 * 중성화 했으면 중성화 한 강아지 대상으로
-		 * 중성화 안했으면 같은 성별끼리만 ,
-		 *  
-		 * 소형    중형       대형
-		 *  <6kg <10kg
-		 * 소형은 중형까지, 중형은 다되는걸로 하고 대형은 중형까지
+		 * 중성화 했으면 중성화 한 강아지 대상으로 중성화 안했으면 같은 성별끼리만 ,
 		 * 
-		 * 나이는
-		 * 1살 미만, 1살 ~ 5살, 5살 ~ 10살, 10살 이상
+		 * 소형 중형 대형 <6kg <10kg 소형은 중형까지, 중형은 다되는걸로 하고 대형은 중형까지
+		 * 
+		 * 나이는 1살 미만, 1살 ~ 5살, 5살 ~ 10살, 10살 이상
 		 */
-		if(weight < 6.0) {
+		if (weight < 6.0) {
 			startWeight = "0";
 			endWeight = "5.9";
 		} else if (weight >= 10.0) {
@@ -254,61 +261,69 @@ public class AppointmentService {
 		}
 		List<Object[]> recList = new ArrayList<Object[]>();
 		List<DogRecommendListDTO> reclist = new ArrayList<DogRecommendListDTO>();
-		
-		if(neutured) { // 중성화 한 강아지 true = 1, false = 0
-			if(age < 1) { // 1살 미만
-				logger.info("11111111111111111111 ============== : {},{}", startWeight,endWeight);
-				recList = appointmentRepository.getNeuturedList(userId, regionCode, oneYearBefore, thisYear, startWeight, endWeight);
-			} else if(age < 5) { // 1~5살
-				logger.info("2222222 ============== : {},{}", startWeight,endWeight);
-				recList = appointmentRepository.getNeuturedList(userId, regionCode, fiveYearBefore, oneYearBefore, startWeight, endWeight);
-			} else if(age < 10) { // 5~10살
-				logger.info("333333333 ============== : {},{}", startWeight,endWeight);
-				recList = appointmentRepository.getNeuturedList(userId, regionCode, tenYearBefore, fiveYearBefore, startWeight, endWeight);
+
+		if (neutured) { // 중성화 한 강아지 true = 1, false = 0
+			if (age < 1) { // 1살 미만
+				logger.info("11111111111111111111 ============== : {},{}", startWeight, endWeight);
+				recList = appointmentRepository.getNeuturedList(userId, regionCode, oneYearBefore, thisYear,
+						startWeight, endWeight);
+			} else if (age < 5) { // 1~5살
+				logger.info("2222222 ============== : {},{}", startWeight, endWeight);
+				recList = appointmentRepository.getNeuturedList(userId, regionCode, fiveYearBefore, oneYearBefore,
+						startWeight, endWeight);
+			} else if (age < 10) { // 5~10살
+				logger.info("333333333 ============== : {},{}", startWeight, endWeight);
+				recList = appointmentRepository.getNeuturedList(userId, regionCode, tenYearBefore, fiveYearBefore,
+						startWeight, endWeight);
 			} else { // 10살 이상
-				logger.info("44444444 ============== : {},{}", startWeight,endWeight);
-				recList = appointmentRepository.getNeuturedList(userId, regionCode, "190001", tenYearBefore, startWeight, endWeight);
+				logger.info("44444444 ============== : {},{}", startWeight, endWeight);
+				recList = appointmentRepository.getNeuturedList(userId, regionCode, "190001", tenYearBefore,
+						startWeight, endWeight);
 			}
 		} else { // 중성화 안한 강아지, 쿼리 문 뒤에 dogGender 붙여야함
-			if(age < 1) { // 1살 미만
-				recList = appointmentRepository.getGenderList(userId, regionCode, oneYearBefore, thisYear, startWeight, endWeight, gender);
-			} else if(age < 5) { // 1~5살
-				recList = appointmentRepository.getGenderList(userId, regionCode, fiveYearBefore, oneYearBefore, startWeight, endWeight, gender);
-			} else if(age < 10) { // 5~10살
-				recList = appointmentRepository.getGenderList(userId, regionCode, tenYearBefore, fiveYearBefore, startWeight, endWeight, gender);
+			if (age < 1) { // 1살 미만
+				recList = appointmentRepository.getGenderList(userId, regionCode, oneYearBefore, thisYear, startWeight,
+						endWeight, gender);
+			} else if (age < 5) { // 1~5살
+				recList = appointmentRepository.getGenderList(userId, regionCode, fiveYearBefore, oneYearBefore,
+						startWeight, endWeight, gender);
+			} else if (age < 10) { // 5~10살
+				recList = appointmentRepository.getGenderList(userId, regionCode, tenYearBefore, fiveYearBefore,
+						startWeight, endWeight, gender);
 			} else { // 10살 이상
-				recList = appointmentRepository.getGenderList(userId, regionCode, "190001", tenYearBefore, startWeight, endWeight, gender);
+				recList = appointmentRepository.getGenderList(userId, regionCode, "190001", tenYearBefore, startWeight,
+						endWeight, gender);
 			}
 		}
-		logger.info("11111111111111111111 ============== : {},{}", oneYearBefore,thisYear);
-		logger.info("11111111111111111111 ============== : {},{}", userId,regionCode);
+		logger.info("11111111111111111111 ============== : {},{}", oneYearBefore, thisYear);
+		logger.info("11111111111111111111 ============== : {},{}", userId, regionCode);
 		logger.info("recommendedList ============== : {}", recList);
-		reclist  = recList.stream().map(r -> DogRecommendListDTO.of(r)).collect(Collectors.toList());
+		reclist = recList.stream().map(r -> DogRecommendListDTO.of(r)).collect(Collectors.toList());
 		return reclist;
 	}
-	
+
 	// 전체 친구 추천
 	public List<DogRecommendListDTO> recommendFriendsForDog(long userId) {
-		List<DogInfoRespDTO> followDogs= followService.getFollowingList(userId);
+		List<DogInfoRespDTO> followDogs = followService.getFollowingList(userId);
 		List<Long> followDogIds = new ArrayList<Long>();
-		
+
 		for (DogInfoRespDTO dog : followDogs) {
 			followDogIds.add(dog.getDogId());
 		}
-		
+
 		List<Object[]> recList = new ArrayList<Object[]>();
 		List<DogRecommendListDTO> reclist = new ArrayList<DogRecommendListDTO>();
-		
+
 		if (followDogIds.size() == 0) {
 			followDogIds.add(0L);
 		}
 		logger.info("followDogIds ============== : {}", followDogIds);
 		recList = appointmentRepository.getDogAllList(userId, followDogIds);
 		logger.info("recommendedList ============== : {}", recList);
-		reclist  = recList.stream().map(r -> DogRecommendListDTO.of(r)).collect(Collectors.toList());
+		reclist = recList.stream().map(r -> DogRecommendListDTO.of(r)).collect(Collectors.toList());
 		return reclist;
 	}
-	
+
 	public Appointment findAppointmentById(long roomId) {
 		return appointmentRepository.findById(roomId).orElse(null);
 	}
