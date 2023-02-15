9ceb7730ee78265513bfcc7f16fa968b5b8e3dd2
Da Yeon Sung <sdy32@naver.com>
2023-02-15 17:13:00 +0900

    #hotfix

--- togetdog_client/src/pages/Feed.js
+++ togetdog_client/src/pages/Feed.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import ConfirmModal from '../components/ConfirmModal';
@@ -91,7 +91,7 @@ const Feed = () => {
   const feedUserId = location.pathname.split('/').reverse()[0];
 
   // pageNo
-  const pageNo = 1;
+  const pageNo = useRef(1);
 
   const swapMainDog = async (targetDogId) => {
     setLoading(true);
